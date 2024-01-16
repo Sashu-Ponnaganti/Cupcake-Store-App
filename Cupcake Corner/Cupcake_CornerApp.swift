7eaaad615c9172bb6c90d83da25a5b8678df5120
Sashu-Ponnaganti <88560514+Sashu-Ponnaganti@users.noreply.github.com>
2024-01-15 19:59:36 -0500

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Sashu Ponnaganti on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
